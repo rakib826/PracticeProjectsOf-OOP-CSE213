46874d9946e705ff12ea15c65122cd81f65ba9bf
Kazi Md. Rakibul Hassan <2220936@iub.edu.bd>
2024-06-05 23:14:00 +0600

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.oop.milkvitapersonal.oopmilkvitapersonal {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.oop.milkvitapersonal.oopmilkvitapersonal to javafx.fxml;
-    exports com.oop.milkvitapersonal.oopmilkvitapersonal;
-}
