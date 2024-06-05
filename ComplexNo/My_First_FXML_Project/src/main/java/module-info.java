2b2220abca6a706340df038da7d246d1317141e7
Kazi Md. Rakibul Hassan <2220936@iub.edu.bd>
2024-06-05 23:13:41 +0600

    Delete ComplexNo directory

--- ComplexNo/My_First_FXML_Project/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.tabriji.tutorial_2.my_first_fxml_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.tabriji.tutorial_2.my_first_fxml_project to javafx.fxml;
-    exports com.tabriji.tutorial_2.my_first_fxml_project;
-}
